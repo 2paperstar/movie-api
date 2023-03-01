c45470350ece62f61400705f955f26ced63e9a3c
2paperstar <lee@paperst.ar>
2023-03-02 01:26:58 +0900

    fix: api version

--- docs/docs.go
+++ docs/docs.go
@@ -149,7 +149,7 @@ const docTemplate = `{
 
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
-	Version:          "1.0.0",
+	Version:          "1.0.1",
 	Host:             "test.paperst.ar",
 	BasePath:         "",
 	Schemes:          []string{},
