a2a4c44f14bb722868ce3f2a41c4d72dc350d5f6
2paperstar <lee@paperst.ar>
2023-03-05 15:00:40 +0900

    fix: allow cors

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	_ "github.com/2paperstar/movie-api/docs"
 	"github.com/2paperstar/movie-api/router"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	swaggerFiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
@@ -15,6 +16,7 @@ import (
 // @host test.paperst.ar
 func setup() *gin.Engine {
 	r := router.SetupRouter()
+	r.Use(cors.Default())
 	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 	return r
 }
