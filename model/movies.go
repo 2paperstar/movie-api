56daea6cf3836e7e7a3086cdf2245d0afdcc6640
2paperstar <lee@paperst.ar>
2023-03-06 00:17:44 +0900

    *fix(breaking-change): snake -> camel case

--- model/movies.go
+++ model/movies.go
@@ -7,17 +7,17 @@ type Movie struct {
 	Title        string   `json:"title"`
 	Director     *string  `json:"director"`
 	Genre        []string `json:"genre"`
-	PosterURL    string   `json:"poster_url"`
-	ReviewCounts int      `json:"review_counts"`
-	ReviewScore  float32  `json:"review_score"`
+	PosterURL    string   `json:"posterUrl"`
+	ReviewCounts int      `json:"reviewCounts"`
+	ReviewScore  float32  `json:"reviewScore"`
 	Story        string   `json:"story,omitempty"`
 }
 
 type Comment struct {
 	ID        string    `json:"id"`
-	MovieID   string    `json:"movie_id"`
+	MovieID   string    `json:"movieId"`
 	Content   string    `json:"content"`
 	Author    string    `json:"author"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
+	CreatedAt time.Time `json:"createdAt"`
+	UpdatedAt time.Time `json:"updatedAt"`
 }
