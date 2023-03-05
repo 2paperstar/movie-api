8593c2c0e9e8a068c326c78583b28caa16e31280
2paperstar <lee@paperst.ar>
2023-03-05 19:00:59 +0900

    fix: 제발 돼라

--- main.go
+++ main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-	_ "github.com/2paperstar/movie-api/docs"
+	// _ "github.com/2paperstar/movie-api/docs"
 	"github.com/2paperstar/movie-api/router"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
-	fiberSwagger "github.com/swaggo/fiber-swagger"
+	// fiberSwagger "github.com/swaggo/fiber-swagger"
 )
 
 // @title Movie API
@@ -17,6 +17,6 @@ func setup() *fiber.App {
 	app := fiber.New()
 	app.Use(cors.New())
 	router.SetupRouter(app)
-	app.Get("/swagger/*", fiberSwagger.WrapHandler)
+	// app.Get("/swagger/*", fiberSwagger.WrapHandler)
 	return app
 }
